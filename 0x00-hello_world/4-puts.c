72ea67efd01ea09708fd85f533c098fb47321f9b
Jenniscott <scottbellojennifer.com>
2023-09-07 09:13:48 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 /**
 * main - print the string in the put function
 * Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle" using puts
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
