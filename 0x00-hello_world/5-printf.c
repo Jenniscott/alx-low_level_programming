8ec5160621174455bf2d35e8e49ed3db27d4a2aa
Jenniscott <scottbellojennifer.com>
2023-09-07 09:28:33 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the characters in the printf function 
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
