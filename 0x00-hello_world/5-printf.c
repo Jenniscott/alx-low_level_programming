d1b22ca8687afca3b25a16d6afc3f0ff15ac1304
Jenniscott <scottbellojennifer.com>
2023-09-07 09:43:20 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - Entry point 
- * Description: prints the characters inthe function printf 
- * return: Always 0 (Sucess) 
+ * main - Entry point
+ * Description: prints the characters inthe function printf
+ * return: Always 0 (Sucess)
  */
 int main(void)
 {
