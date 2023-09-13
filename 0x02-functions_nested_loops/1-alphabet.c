ca530ca2f693fff63475d32f1399eabd8a924942
Jenniscott <scottbellojennifer.com>
2023-09-13 15:05:46 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_alphabet - Entry point
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+
+		_putchar(c);
+		c++;
+	}
+		_putchar("\n")
+		return (0);
+}
+
