ac8f98187c158caa64716a04fad80480ae937e19
Jenniscott <scottbellojennifer.com>
2023-09-15 16:37:56 -0700

    Please work

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,16 @@
 
 /**
  * _isupper - uppercase letters
- * @c: character to check
- *
+ * @c: character to check 
  * Return: 0 or 1
  */
 
 int _isupper(int c)
 {
 
-	if (c <= 'A' && c >= 'Z')
-	{
+	if (c <= 65 && c >= 90)
+ 		{
 		return (1);
-	}
+		}
 		return (0);
 }
