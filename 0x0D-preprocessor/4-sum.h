8e6f4a189fd17cd72004b7e684fa9464ae983141
Jenniscott <scottbellojennifer@gmail.com>
2023-11-07 14:57:56 -0800

    Sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUM_H_
+#define SUM_H_
+
+/**
+ * main - sums two value
+ *
+ * @x: first value
+ * @y: second value
+ *
+ * Desc:header file that computes the sum of x and y
+ */
+
+#define SUM(x, y) (x + y)
+
+#endif
