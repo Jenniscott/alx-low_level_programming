a750dfc6e52dd6f171917b3dbbccf025d8e16f44
Jenniscott <scottbellojennifer@gmail.com>
2023-11-07 14:44:41 -0800

    absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_LIKE_MACRO_H_
+#define FUNCTION_LIKE_MACRO_H_
+
+/**
+ * main - compute absolute value
+ *
+ * @x: input value
+ *
+ * Desc: header file that compute abslute value
+ */
+
+#define ABS(x) ((x) < 0 ? (-1) * (x) : (x))
+
+#endif
