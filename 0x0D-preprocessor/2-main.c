d3deb9883ff968c19e9d20ba2d2e49ba9783d8cd
Jenniscott <scottbellojennifer@gmail.com>
2023-11-07 14:01:17 -0800

    filename

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,11 +3,10 @@
 /**
  * main - prints the name of the file
  *
- * Return: Always 0 (sucess)
+ * Return: Always 0 (Sucess)
  */
-
 int main(void)
 {
 	printf("%s\n", _FILE_);
-		return (0);
+	return (0);
 }
