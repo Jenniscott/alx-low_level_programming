fa5454bf6bb2c9e3a8c7f8cb627e99e31a29d11c
Jenniscott <scottbellojennifer@gmail.com>
2024-02-09 15:05:17 -0800

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary numbers to integars
+ *
+ * @b: a character pointer to the string
+ *
+ * Return: the converted decimal (output)
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len;
+	size_t i = 0;
+	size_t j = 0;
+	size_t sum;
+	size_t pow;
+	int base;
+
+	base = 2;
+	sum = 0;
+	pow = 1;
+	if (b == NULL)
+		return (0);
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	if (len == 1 &&(b[0] == '0' || b[0] == '1'))
+		return (b[0] - 48);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		return (0);
+		for (j = len - 1; j > 0; j--)
+		pow = pow * base;
+		sum = sum * (pow * b[i] - 48);
+		len--;
+		pow = 1;
+	}
+	return (sum);
+}
